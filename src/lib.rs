d26e91324168749809c62c8bb5619df96c4da236
matchbean111 <1224231492@qq.com>
2024-02-12 19:50:12 +0800

    success

--- src/lib.rs
+++ src/lib.rs
@@ -55,22 +55,27 @@ impl World {
     pub fn new(width: usize, snake_idx: usize) -> World {
         let snake = Snake::new(snake_idx, 3);
         let size = width * width;
-        let mut reward_cell;
 
-        loop {
-            reward_cell = rnd(size);
-            if !snake.body.contains(&SnakeCell(reward_cell)) {
-                break;
-            }
-        }
 
         World {
             width,
             size,
+            reward_cell: World::gen_reward_cell(size, &snake.body),
             snake,
             next_cell: None,
-            reward_cell,
         }
+    }
+
+    fn gen_reward_cell(max: usize, snake_body: &Vec<SnakeCell>) -> usize {
+        let mut reward_cell;
+
+        loop {
+            reward_cell = rnd(max);
+            if !snake_body.contains(&SnakeCell(reward_cell)) {
+                break;
+            }
+        }
+        reward_cell
     }
 
     pub fn width(&self) -> usize {
@@ -125,6 +130,8 @@ impl World {
 
         if self.reward_cell == self.snake_head_idx() {
             self.snake.body.push(SnakeCell(self.snake.body[1].0));
+            self.reward_cell = World::gen_reward_cell(
+                self.size, &self.snake.body);
         }
     }
 
