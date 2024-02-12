0193f95a71870664e8a223165bd36676091eefdc
matchbean111 <1224231492@qq.com>
2024-02-12 19:41:50 +0800

    this is success

---
+++ src/lib.rs
@@ -0,0 +1,172 @@
+use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+#[wasm_bindgen(module = "/www/utils/rnd.js")]
+extern "C" {
+    fn rnd(max: usize) -> usize;
+}
+
+#[wasm_bindgen]
+#[derive(PartialEq)]
+pub enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+pub struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+    direction: Direction,
+}
+
+impl Snake {
+    fn new(spawn_index: usize, size: usize) -> Snake {
+        let mut body = vec![];
+
+        for i in 0..size {
+            body.push(SnakeCell(spawn_index - i));
+        }
+
+        Snake {
+            body,
+            direction: Direction::Right,
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct World {
+    width: usize,
+    size: usize,
+    snake: Snake,
+    next_cell: Option<SnakeCell>,
+    reward_cell: usize,
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new(width: usize, snake_idx: usize) -> World {
+        let snake = Snake::new(snake_idx, 3);
+        let size = width * width;
+        let mut reward_cell;
+
+        loop {
+            reward_cell = rnd(size);
+            if !snake.body.contains(&SnakeCell(reward_cell)) {
+                break;
+            }
+        }
+
+        World {
+            width,
+            size,
+            snake,
+            next_cell: None,
+            reward_cell,
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn reward_cell(&self) -> usize {
+        self.reward_cell
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
+
+    pub fn change_snake_dir(&mut self, direction: Direction) {
+        let next_cell = self.gen_next_snake_cell(&direction);
+
+        if self.snake.body[1].0 == next_cell.0 {
+            return;
+        }
+
+        self.next_cell = Some(next_cell);
+        self.snake.direction = direction;
+    }
+
+    pub fn snake_length(&self) -> usize {
+        self.snake.body.len()
+    }
+
+    pub fn snake_cells(&self) -> *const SnakeCell {
+        self.snake.body.as_ptr()
+    }
+
+    pub fn step(&mut self) {
+        let temp = self.snake.body.clone();
+
+        match self.next_cell {
+            Some(cell) => {
+                self.snake.body[0] = cell;
+                self.next_cell = None;
+            }
+            None => {
+                self.snake.body[0] = self.gen_next_snake_cell(&self.snake.direction);
+            }
+        }
+
+        let len = self.snake.body.len();
+
+        for i in 1..len {
+            self.snake.body[i] = SnakeCell(temp[i - 1].0);
+        }
+
+        if self.reward_cell == self.snake_head_idx() {
+            self.snake.body.push(SnakeCell(self.snake.body[1].0));
+        }
+    }
+
+    fn gen_next_snake_cell(&self, direction: &Direction) -> SnakeCell {
+        let snake_idx = self.snake_head_idx();
+        let row = snake_idx / self.width;
+
+        match direction {
+            Direction::Right => {
+                let treshold = (row + 1) * self.width;
+                if snake_idx + 1 == treshold {
+                    SnakeCell(treshold - self.width)
+                } else {
+                    SnakeCell(snake_idx + 1)
+                }
+            }
+            Direction::Left => {
+                let treshold = row * self.width;
+                if snake_idx == treshold {
+                    SnakeCell(treshold + (self.width - 1))
+                } else {
+                    SnakeCell(snake_idx - 1)
+                }
+            }
+            Direction::Up => {
+                let treshold = snake_idx - (row * self.width);
+                if snake_idx == treshold {
+                    SnakeCell((self.size - self.width) + treshold)
+                } else {
+                    SnakeCell(snake_idx - self.width)
+                }
+            }
+            Direction::Down => {
+                let treshold = snake_idx + ((self.width - row) * self.width);
+                if snake_idx + self.width == treshold {
+                    SnakeCell(treshold - ((row + 1) * self.width))
+                } else {
+                    SnakeCell(snake_idx + self.width)
+                }
+            }
+        }
+    }
+}
+
+// wasm-pack build --target web
